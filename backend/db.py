dcf78e5a5e5de55ed51ab723236f1abde897e3b9
nicoaraujoo07 <naraujo@fi.uba.ar>
2025-11-22 13:57:35 -0300

    fix: arreglo sintaxis

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,11 @@
 #el comando es "pipenv install mysql-connector-python"
+import mysql.connector
 
 def get_conexion():
     conexion = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="contraseña",
+        password="nicolas745",
         #aca pongan la contraseña q hayan puesto ( pongan si o si una contraseña sino no van a tener acceso )
         database="sistema_reservas",
         #aca pongan el nombre de su base de datos
